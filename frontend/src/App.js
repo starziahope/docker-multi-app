b254b0b6a64a94f359083a1c0e9fd6e65e65671a
starziahope <ddddasd1@naver.com>
2021-12-04 21:10:35 +0900

    travis file added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -56,7 +56,7 @@ function App() {
             <li key={index}>{list.value} </li>
           ))}
           <br />
-            안녕하세요.
+            안녕하세요..
           <form className="example" onSubmit={submitHandler}>
             <input
               type="text"
